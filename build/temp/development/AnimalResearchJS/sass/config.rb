483d8ff93f509bd027cb5c3066fa4ba55f704d4b
joelchittum <joel.chittum@gmail.com>
2018-02-05 17:33:19 -0500

    Auto Gen App

---
+++ build/temp/development/AnimalResearchJS/sass/config.rb
@@ -0,0 +1,1 @@
+cache_path = 'C:\Users\Joel\Documents\GitHub\AnimalResearchJS/build/.sass-cache'
---
+++ build/temp/development/AnimalResearchJS/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require C:\Users\Joel\Documents\GitHub\AnimalResearchJS\app.js
